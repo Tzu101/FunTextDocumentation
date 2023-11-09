50e8510bf18af1f8860b727703ae17fc20fe6790
Tzu1 <val.waldner5@gmail.com>
2023-11-09 16:57:35 +0100

    Updated funtext version

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import { FunText, type AnimationId, type InputAnimation } from "funtext";
 
+// Dark mode support
+FunText.options = {
+  accessibility: {
+    prefersColorScheme: true,
+  },
+  css: {
+    text: "color: rgb(19, 19, 25);",
+  },
+  altcss: {
+    text: "color: rgb(228, 228, 240);",
+  },
+};
+
 // Page title
 const funtext_title_container = document.getElementById("funtext");
 const funtext_title_animations: InputAnimation[] = [
   {
     scope: "letter",
     property: "color",
-    duration: 1.15,
+    duration: 1.05,
     steps: ["rgb(47, 212, 47)", "rgb(25, 128, 25)", "rgb(47, 212, 47)"],
     iteration: "infinite",
     sync: {
