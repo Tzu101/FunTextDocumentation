a07d0f35a464365bb8ad24101359896862044731
Tzu1 <val.waldner5@gmail.com>
2023-11-09 09:29:32 +0100

    Setup example

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
 import { FunText, type AnimationId, type InputAnimation } from "funtext";
 
-const test: AnimationId = {
-  scope: 0,
-  property: "",
-};
-
-const funtext_container = document.getElementById("funtext");
-const funtext_animations: InputAnimation[] = [
+// Page title
+const funtext_title_container = document.getElementById("funtext");
+const funtext_title_animations: InputAnimation[] = [
   {
     scope: "letter",
     property: "color",
     duration: 1.15,
-    steps: [
-      "rgb(228, 228, 240)",
-      "rgb(47, 212, 47)",
-      "rgb(25, 128, 25)",
-      "rgb(47, 212, 47)",
-      "rgb(228, 228, 240)",
-    ],
+    steps: ["rgb(47, 212, 47)", "rgb(25, 128, 25)", "rgb(47, 212, 47)"],
     iteration: "infinite",
     sync: {
       location: 0,
@@ -26,9 +16,32 @@ const funtext_animations: InputAnimation[] = [
     offset: 0.25,
   },
 ];
+if (funtext_title_container) {
+  const funtext_title = new FunText(
+    funtext_title_container,
+    funtext_title_animations
+  );
+  funtext_title.mount();
+}
 
-if (funtext_container) {
-  const text1 = new FunText(funtext_container, funtext_animations);
-  text1.mount();
-  text1.pause({ scope: 1, property: "" });
+// Setup example
+const funtext_example_setup_container = document.getElementById(
+  "funtext_example_setup"
+);
+const funtext_example_setup_animations: InputAnimation[] = [
+  {
+    scope: "letter",
+    property: "translate",
+    duration: 1.5,
+    steps: "0 10px",
+    iteration: "infinite",
+    direction: "alternate",
+  },
+];
+if (funtext_example_setup_container) {
+  const funtext_example_setup = new FunText(
+    funtext_example_setup_container,
+    funtext_example_setup_animations
+  );
+  funtext_example_setup.mount();
 }
